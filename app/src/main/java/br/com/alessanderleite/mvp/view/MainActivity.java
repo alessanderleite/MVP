dbf1fecc9ac53474c20a88a3813d0d8caea0178d
alessanderleite <alessander.logistica@gmail.com>
2019-04-05 14:19:11 -0300

    refactoring the MainActivity

--- app/src/main/java/br/com/alessanderleite/mvp/view/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/mvp/view/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.alessanderleite.mvp;
+package br.com.alessanderleite.mvp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.alessanderleite.mvp.R;
 
 public class MainActivity extends AppCompatActivity {
 
